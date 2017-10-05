1e085d122b8d2e4ace3623b10e6d5099d0c8c097
Rais <rais.gowa@gmail.com>
2017-10-05 07:38:01 +0700

    Initial setup SpringBoot Application

---
+++ src/main/java/rais/friendmanagement/Application.java
@@ -0,0 +1,16 @@
+package rais.friendmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Muhammad Rais Rahim <rais.gowa@gmail.com>
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
